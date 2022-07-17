38d50ba064bc6b8b2b371bb5f7f47f4b57430462
Ryan Wahle <ryanwahle8@gmail.com>
2022-07-17 14:14:32 -0500

    UI finished

--- code.js
+++ code.js
@@ -4,6 +4,39 @@ let cWins = 0;
 let count = 0;
 
 
+let body = document.querySelector('.body');
+let rock = document.querySelector('.rock');
+let paper = document.querySelector('.paper');
+let scissors = document.querySelector('.scissors');
+let results = document.querySelector('.results');
+let score = document.querySelector('.score');
+let winner = document.querySelector('.winner');
+let restart = document.createElement("button");
+let buttonContainer = document.querySelector(".buttonContainer")
+restart.textContent = "Play Again?";
+restart.addEventListener('click', () => {
+    window.location.reload();
+});
+restart.classList.add("restart");
+
+
+rock.addEventListener('click', function(){
+    playRound('rock', computerPlay());
+});
+
+paper.addEventListener('click', function(){
+    playRound('paper', computerPlay());
+});
+
+scissors.addEventListener('click', function(){
+    playRound('scissors', computerPlay());
+});
+
+function updateScore(){
+    score.textContent = pWins + " - " + cWins;
+}
+
+
 function computerPlay(){
     num = Math.floor(1 + 3 * Math.random());
     switch(num){
@@ -22,22 +55,38 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if(playerSelection == computerSelection){
-        return "Its a tie! Both sides chose " + playerSelection;
+        results.textContent = "Its a tie! Both sides chose " + playerSelection
     } else if ((playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
         cWins++;
-        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        results.textContent = ("You Lose! " + computerSelection + " beats " + playerSelection);
     } else{
         pWins++;
-        return "You Win! " + playerSelection + " beats " + computerSelection;
+        results.textContent = ("You Win! " + playerSelection + " beats " + computerSelection);
     } 
+    updateScore();
+    if(!(pWins < 5)){
+        winner.textContent = "Congratualations! You beat the computer due to sheer luck!";
+        rock.replaceWith(rock.cloneNode(true));
+        paper.replaceWith(paper.cloneNode(true));
+        scissors.replaceWith(scissors.cloneNode(true));
+        buttonContainer.appendChild(restart);
+    } else if(!(cWins < 5)){
+        winner.textContent = "Humans are doomed; the CPU was victorious.";
+        rock.replaceWith(rock.cloneNode(true));
+        paper.replaceWith(paper.cloneNode(true));
+        scissors.replaceWith(scissors.cloneNode(true));
+        buttonContainer.appendChild(restart);
+    } 
+
 }
 
-function game(){
+
+/* function game(){
     while(pWins < 3 && cWins < 3){
         let input = prompt('Game ' + (count + 1) + ': Enter "Rock", "Paper", or "Scissors"')
-        console.log(playRound(input, computerPlay(), cWins, pWins));
+        console.log(playRound(input, computerPlay()));
         console.log("Your Win Total is: " + pWins);
         console.log("CPU win Total is: " + cWins);
         count++;
     }
-}
+} */
